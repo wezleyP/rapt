614776823220b89981f61affea2af87c0019d276
Wesley Patterson <81829921+wezleyP@users.noreply.github.com>
2023-06-26 20:18:46 -0500

    Initial Commit

---
+++ rapt/raptApp.swift
@@ -0,0 +1,17 @@
+//
+//  raptApp.swift
+//  rapt
+//
+//  Created by Wesley Patterson on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct raptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
